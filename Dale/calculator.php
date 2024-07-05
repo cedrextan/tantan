928a3bbcf8f7354a70959a3f338a17d49f93f377
cedrextan <tcedrex@gmail.com>
2024-07-05 19:16:31 +0800

    Add files via upload

---
+++ Dale/calculator.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculator</title>
+    <link rel="stylesheet" href="calculators.css">
+</head>
+<body>
+    <div class="calculator">
+        <div class="display" id="display"></div>
+        <div class="buttons">
+            </div>
+    </div>
+    <script src="calculators.js"></script>
+
+    <div class="link">
+        <a href="index.php">Go back</a>
+    </div>
+    <div class="imgs">
+        <img src="images/logo2.png">
+    </div>
+</body>
+</html>
